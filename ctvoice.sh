f17316acc12c0549cdf15bdfffa1e693aad553d9
Jannick Fahlbusch <jannick@famfahlbusch.de>
2013-10-10 16:01:53 +0200

    Remove repeating of spoken commands

--- ctvoice.sh
+++ ctvoice.sh
@@ -48,7 +48,7 @@ if [ $size -gt $lastsize ]
 				ffmpeg -loglevel panic -y -i test.wav -ar 16000 -acodec flac file.flac
 				wget -q -U "Mozilla/5.0" --post-file file.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v1/recognize?lang=de-de&client=chromium" | cut -d\" -f12 >stt.txt
 				
-				say "$(cat stt.txt)"
+			
 
 				if [[ $(cat stt.txt) =~ "weiter" ]]
 				then
