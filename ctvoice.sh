733f6650305d67ea8979a8c037b0caa1cb9f9504
Jannick Fahlbusch <jannick@famfahlbusch.de>
2013-10-10 16:26:01 +0200

    Replace '&' with 'and'

--- ctvoice.sh
+++ ctvoice.sh
@@ -16,12 +16,20 @@ function say-en {
 mplayer -ao alsa:device=hw=0.0 -really-quiet -http-header-fields "User-Agent:Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.172 Safari/537.22m" "http://translate.google.com/translate_tts?tl=en&q=$1";
 }
 function say-title {
-mpc pause 
+mpc pause
+ 
 say-en "$(mpc current)"
 mpc play
 }
-
-
+function remove_and {
+current=$(mpc current)
+#Das kaufmännische und in ein normales und verwandeln 
+if [ -n "`echo $current | grep "&"`" ]; then
+	echo "Ersetze kaufmännisches und."
+	current=$(echo $current | sed 's/&/and/g')
+fi
+echo $current
+}
 
 sox -t alsa hw:1,0 test.wav silence 1 0 0.5% -1 1.0 1% &
 sox_pid=$!
